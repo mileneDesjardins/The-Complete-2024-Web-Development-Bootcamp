80a49c866da7d3d7ce6b03cc8448f02070e15274
Milene Desjardins <m.desjardins31@outlook.com>
2023-10-15 18:13:21 -0400

    Ternary, AND operator

--- conditional-rendering/src/components/App.jsx
+++ conditional-rendering/src/components/App.jsx
@@ -1,27 +1,21 @@
 import React from "react";
+import Login from "./Login";
 
 var isLoggedin = true;
 
-function renderConditionally() {
-  if (isLoggedin === true) {
-    return <h1>Hello</h1>;
-  } else {
-    return(
-    <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    );
-  }
-}
+const currentTime = new Date(2019, 11 ,1, 8).getHours();
+
+
 
 function App() {
-  return (
-    <div className="container">
-      {renderConditionally()}
-    </div>
-  );
+  return <div className="container">{
+    //Ternary
+    //isLoggedin ? <h1>Hello</h1> : <Login />
+
+    //AND Operator
+    //currentTime > 12 && <h1>Why are you still working</h1>
+    
+  }</div>;
 }
 
 export default App;
