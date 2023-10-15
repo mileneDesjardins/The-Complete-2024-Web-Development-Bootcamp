78d34700117c21af1998383ee4aaa89875398e0e
Milene Desjardins <m.desjardins31@outlook.com>
2023-10-15 18:30:52 -0400

    conditional rendering practice

---
+++ conditional-rendering-practice/src/components/Form.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Form({isRegistered}) {
+  return (
+    <form className="form">
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      {!isRegistered && <input type="password" placeholder="Confirm Password" />}
+      
+      <button type="submit">
+        {isRegistered ? "Login" : "Registered"}
+      </button>
+    </form>
+  );
+}
+
+export default Form;
