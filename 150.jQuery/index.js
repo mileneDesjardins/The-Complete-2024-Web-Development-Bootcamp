ce1690024cc84dcc7321a5e0e7b7eb13257ad10c
Milene Desjardins <m.desjardins31@outlook.com>
2024-07-04 14:53:07 -0400

    projet

--- 150.jQuery/index.js
+++ 150.jQuery/index.js
@@ -1,3 +1,5 @@
 // But now all you need to know is that we're using this to select the h1 on our web page and we're changing its CSS, namely the color property, to red.
 
-$("h1").css("color", "red");
+$("h1").text("bye");
+
+$("button").html("<em>Hey</em");
