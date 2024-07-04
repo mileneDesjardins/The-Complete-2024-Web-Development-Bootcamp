ead0404d27eb6075cf03bff3eaf4a5ee0c04d97b
Milene Desjardins <m.desjardins31@outlook.com>
2024-07-04 12:12:30 -0400

    jQuery

---
+++ 150.jQuery/index.js
@@ -0,0 +1,3 @@
+// But now all you need to know is that we're using this to select the h1 on our web page and we're changing its CSS, namely the color property, to red.
+
+$("h1").css("color", "red");
