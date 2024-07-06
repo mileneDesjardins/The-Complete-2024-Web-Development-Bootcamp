343659ed4a16b8696feae3b88c9a614500a4f09b
Milene Desjardins <m.desjardins31@outlook.com>
2024-07-06 11:52:33 -0400

    sound

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -6,7 +6,11 @@ function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 3);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+
   $("#" + randomChosenColour).fadeTo(100, 0.3, function () {
     $(this).fadeTo(500, 1.0);
   });
+
+  var colorAudio = new Audio("sounds\\" + randomChosenColour + ".mp3");
+  colorAudio.play();
 }
